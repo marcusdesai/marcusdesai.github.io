f9c4a0bef9c2c1f738d83712864de5ec3d653e7e
Marcus Desai <marcus.desai@gmail.com>
2023-07-29 17:17:21 +0100

    fix publishconf imports

--- publishconf.py
+++ publishconf.py
@@ -1,4 +1,8 @@
-# noinspection PyUnresolvedReferences
+import sys
+import os
+
+sys.path.append(os.curdir)
+# noinspection PyUnresolvedReferences,PyPackages
 from pelicanconf import *
 
 SITEURL = "https://blog.mrcsd.com"
